37ee33ba31eec2c1c6372ddee9843451a7982fcb
Jared Hanson <jrodhanson@gmail.com>
2021-03-23 13:21:47 -0600

    All my integration tests

---
+++ app/src/test/java/edu/byu/cs/tweeter/model/integration/Integration_FollowerTest.java
@@ -0,0 +1,157 @@
+package edu.byu.cs.tweeter.model.integration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.model.service.FollowDataServiceProxy;
+import edu.byu.cs.tweeter.client.model.service.FollowerServiceProxy;
+import edu.byu.cs.tweeter.client.model.service.FollowingServiceProxy;
+import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.service.request.FollowDataRequest;
+import edu.byu.cs.tweeter.model.service.request.FollowerRequest;
+import edu.byu.cs.tweeter.model.service.request.FollowingRequest;
+import edu.byu.cs.tweeter.model.service.response.FollowDataResponse;
+import edu.byu.cs.tweeter.model.service.response.FollowerResponse;
+import edu.byu.cs.tweeter.model.service.response.FollowingResponse;
+
+public class Integration_FollowerTest {
+    private FollowerRequest validRequest;
+    private FollowerRequest invalidRequest;
+
+    private FollowerResponse successResponse;
+
+
+    private FollowerServiceProxy followingServiceSpy;
+
+
+
+
+
+
+    private static final String MALE_IMAGE_URL = "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png";
+    private static final String FEMALE_IMAGE_URL = "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png";
+
+    private final User user1 = new User("Allen", "Anderson", MALE_IMAGE_URL);
+    private final User user2 = new User("Amy", "Ames", FEMALE_IMAGE_URL);
+    private final User user3 = new User("Bob", "Bobson", MALE_IMAGE_URL);
+    private final User user4 = new User("Bonnie", "Beatty", FEMALE_IMAGE_URL);
+    private final User user5 = new User("Chris", "Colston", MALE_IMAGE_URL);
+    private final User user6 = new User("Cindy", "Coats", FEMALE_IMAGE_URL);
+    private final User user7 = new User("Dan", "Donaldson", MALE_IMAGE_URL);
+    private final User user8 = new User("Dee", "Dempsey", FEMALE_IMAGE_URL);
+    private final User user9 = new User("Elliott", "Enderson", MALE_IMAGE_URL);
+    private final User user10 = new User("Elizabeth", "Engle", FEMALE_IMAGE_URL);
+    private final User user11 = new User("Frank", "Frandson", MALE_IMAGE_URL);
+    private final User user12 = new User("Fran", "Franklin", FEMALE_IMAGE_URL);
+    private final User user13 = new User("Gary", "Gilbert", MALE_IMAGE_URL);
+    private final User user14 = new User("Giovanna", "Giles", FEMALE_IMAGE_URL);
+    private final User user15 = new User("Henry", "Henderson", MALE_IMAGE_URL);
+    private final User user16 = new User("Helen", "Hopwell", FEMALE_IMAGE_URL);
+    private final User user17 = new User("Igor", "Isaacson", MALE_IMAGE_URL);
+    private final User user18 = new User("Isabel", "Isaacson", FEMALE_IMAGE_URL);
+    private final User user19 = new User("Justin", "Jones", MALE_IMAGE_URL);
+    private final User user20 = new User("Jill", "Johnson", FEMALE_IMAGE_URL);
+
+    private List<User> loginFollowees = Arrays.asList(user1, user2, user3, user4, user5, user6, user7,
+            user9, user10, user11, user12, user13, user14, user15, user16, user17, user18,
+            user19, user20);
+
+
+    /**
+     * Create a FollowingService spy that uses a mock ServerFacade to return known responses to
+     * requests.
+     */
+    @BeforeEach
+    public void setup() throws IOException, TweeterRemoteException {
+        User currentUser = new User("FirstName", "LastName",
+                "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png");
+
+        // Setup request objects to use in the tests
+        validRequest = new FollowerRequest(currentUser.getAlias(), 10, null);
+
+
+        // Setup a mock ServerFacade that will return known responses
+        successResponse = getFollowers(validRequest);
+
+
+
+        // Create a FollowingService instance and wrap it with a spy that will use the mock service
+        followingServiceSpy = Mockito.spy(new FollowerServiceProxy());
+
+    }
+
+    @Test
+    public void testGetFollowerData_validRequest_correctResponse() throws IOException, TweeterRemoteException {
+        FollowerResponse response = followingServiceSpy.getFollowers(validRequest);
+        Assertions.assertEquals(successResponse, response);
+    }
+
+
+    List<User> getDummyFollowees() {
+        return Arrays.asList(user1, user2, user3, user4, user5, user6, user7,
+                user8, user9, user10, user11, user12, user13, user14, user15, user16, user17, user18,
+                user19, user20);
+    }
+    public FollowerResponse getFollowers(FollowerRequest request) {
+        // TODO: Generates dummy data. Replace with a real implementation.
+        assert request.getLimit() > 0;
+        assert request.getFollowerAlias() != null;
+
+        List<User> allFollowees = getDummyFollowees();
+        List<User> responseFollowees = new ArrayList<>(request.getLimit());
+
+        boolean hasMorePages = false;
+
+        if(request.getLimit() > 0) {
+            if (allFollowees != null) {
+                int followeesIndex = getFolloweesStartingIndex(request.getLastFolloweeAlias(), allFollowees);
+
+                for(int limitCounter = 0; followeesIndex < allFollowees.size() && limitCounter < request.getLimit(); followeesIndex++, limitCounter++) {
+                    responseFollowees.add(allFollowees.get(followeesIndex));
+                }
+
+                hasMorePages = followeesIndex < allFollowees.size();
+            }
+        }
+
+        return new FollowerResponse(responseFollowees, hasMorePages);
+    }
+
+    /**
+     * Determines the index for the first followee in the specified 'allFollowees' list that should
+     * be returned in the current request. This will be the index of the next followee after the
+     * specified 'lastFollowee'.
+     *
+     * @param lastFolloweeAlias the alias of the last followee that was returned in the previous
+     *                          request or null if there was no previous request.
+     * @param allFollowees the generated list of followees from which we are returning paged results.
+     * @return the index of the first followee to be returned.
+     */
+    private int getFolloweesStartingIndex(String lastFolloweeAlias, List<User> allFollowees) {
+
+        int followeesIndex = 0;
+
+        if(lastFolloweeAlias != null) {
+            // This is a paged request for something after the first page. Find the first item
+            // we should return
+            for (int i = 0; i < allFollowees.size(); i++) {
+                if(lastFolloweeAlias.equals(allFollowees.get(i).getAlias())) {
+                    // We found the index of the last item returned last time. Increment to get
+                    // to the first one we should return
+                    followeesIndex = i + 1;
+                    break;
+                }
+            }
+        }
+
+        return followeesIndex;
+    }
+}
